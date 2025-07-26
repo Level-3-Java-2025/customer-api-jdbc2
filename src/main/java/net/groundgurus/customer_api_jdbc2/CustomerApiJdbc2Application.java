63a24392cf2e9848fd663fa60cbedf0186ba7d39
Philip Mark Gutierrez <sensei_slayer@protonmail.com>
2025-07-26 15:34:22 +0800

    init

---
+++ src/main/java/net/groundgurus/customer_api_jdbc2/CustomerApiJdbc2Application.java
@@ -0,0 +1,13 @@
+package net.groundgurus.customer_api_jdbc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApiJdbc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApiJdbc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/groundgurus/customer_api_jdbc2/CustomerApiJdbc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.groundgurus.customer_api_jdbc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApiJdbc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
